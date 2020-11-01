f7f997efdbe94d571bcfe20802488e3a7c3ba7fb
seckinceylan <seckin.ceylan1218@gmail.com>
2020-11-01 11:01:54 -0500

    ready for demo

--- src/test/java/com/library/Runners/CucumberRunner.java
+++ src/test/java/com/library/Runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/library/StepDefinitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@login",
         publish = true
 
